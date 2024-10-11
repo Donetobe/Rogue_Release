3ecc7624021e642010141faf28ca69b9e9d151a3
Donetobe <s2200872@edu.bc.fi>
2024-10-11 12:23:28 +0300

    1.0
    Valmis

---
+++ LunarLander/Program.cs
@@ -0,0 +1,186 @@
+﻿using System.Numerics;
+using ZeroElectric.Vinculum;
+
+namespace LunarLander
+{
+    internal class Lander
+    {
+        static void Main(string[] args)
+        {
+            Lander game = new Lander();
+            game.Init();
+            game.GameLoop();
+        }
+
+        /////////////////////////////////////
+
+        // Pelaajan sijainti
+        // x on aluksen keskikohta
+        // y on aluksen pohja
+        float x = 120;
+        float y = 16;
+
+        // Onko moottori päällä
+        bool engine_on = false;
+
+        // Pelaajan nopeus, polttoaine ja polttonopeus
+        float velocity = 0;
+        float fuel = 100;
+        float fuel_consumption = 10.0f;
+
+        // Laskeutumisalustan katon sijainti y-akselilla. Y kasvaa alaspäin.
+        int landing_y = 125;
+
+        // Ruudunpäivitykseen menevä aika (oletus)
+        float delta_time = 1.0f / 60.0f;
+
+        // Moottorin voimakkuus
+        float acceleration = 20.9f;
+
+        // Painovoiman voimakkuus
+        float gravity = 9.89f;
+
+        // Pelialueen ja ikkunan mittasuhteet
+        int game_width = 240;
+        int game_height = 136;
+
+        int screen_width = 1280;
+        int screen_height = 720;
+        RenderTexture game_screen;
+
+        // TODO: Lisää tekstuuri johon peli piirretään skaalausta varten #
+        Texture texture;
+        void Init()
+        {
+            // TODO: Aloita Raylib ja luo ikkuna. #
+
+            Raylib.InitWindow(screen_width, screen_height, "Lunar");
+            Raylib.SetTargetFPS(60);
+            texture = Raylib.LoadTexture("ship.png");
+            game_screen = Raylib.LoadRenderTexture(game_width, game_height);
+        }
+      
+        void GameLoop()
+        {
+            // TODO: Pyöritä gamelooppia niin kauan
+            // kun Raylibin ikkuna on auki #
+            while(Raylib.WindowShouldClose()==false)
+            {
+                Update();
+                Draw();
+                DrawGameScaled();
+                // TODO: Kun peli on piirretty, skaalaa se isommaksi #
+                
+            }
+            // TODO: Sulje Raylib ikkuna #
+            Raylib.CloseWindow();
+        }
+
+
+        void Update()
+        {
+            delta_time = Raylib.GetFrameTime();
+            // TODO: Kysy Raylibiltä miten pitkään yksi ruudunpäivitys kesti
+             // Kirjoita funktiokutsu 0.0f tilalle. #
+
+            // Lisää painovoiman vaikutus
+            velocity += gravity * delta_time;
+
+            // TODO: Kun pelaaja painaa nappia (esim nuoli ylös)
+            // ja polttoainetta on jäljellä, lisää
+            // kiihtyvyys nopeuteen #
+            if (Raylib.IsKeyPressed(KeyboardKey.KEY_UP))
+            {
+                velocity -= acceleration * delta_time;
+                fuel -= fuel_consumption * delta_time;
+                engine_on = true;
+
+                velocity = velocity - acceleration;
+            }
+            else
+            {
+                engine_on = false;
+            }
+            // Liikuta alusta
+            y += velocity * delta_time;
+
+            if(y >= landing_y)
+            {
+                if (velocity > 40)
+                {
+                    Environment.Exit(0);
+                }
+                velocity = 0;
+
+                
+            }
+        }
+        private void DrawGameScaled()
+        {
+
+            // Tässä piirretään tekstuuri ruudulle
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Raylib.DARKGRAY);
+
+            int draw_width = Raylib.GetScreenWidth();
+            int draw_height = Raylib.GetScreenHeight();
+            float scale = Math.Min((float)draw_width / game_width, (float)draw_height / game_height);
+
+            // Note: when drawing on texture, the Y-axis is
+            //flipped, need to multiply height by -1
+            Rectangle source = new Rectangle(0.0f, 0.0f,
+                game_screen.texture.width,
+                game_screen.texture.height * -1.0f);
+
+            Rectangle destination = new Rectangle(
+                (draw_width - (float)game_width * scale) * 0.5f,
+                (draw_height - (float)game_height * scale) * 0.5f,
+                game_width * scale,
+                game_height * scale);
+
+            Raylib.DrawTexturePro(game_screen.texture,
+                source, destination,
+                new Vector2(0, 0), 0.0f, Raylib.WHITE);
+
+            Raylib.EndDrawing();
+        }
+        void Draw()
+        {
+
+            // TODO: Aloita piirtäminen # 
+            Raylib.BeginTextureMode(game_screen);
+            // TODO: Tyhjennä ruutu mustaksi # 
+            Raylib.ClearBackground(Raylib.BLACK);
+
+            int plat_x = (int)x - 30;
+            int plat_y = landing_y;
+            int plat_w = 60;
+            int plat_h = 10;
+
+
+            // TODO: Piirrä laskeutumisalusta suorakulmiona #
+            // (plat_x, plat_y, plat_w, plat_h)
+            Raylib.DrawRectangle(plat_x, plat_y, plat_w, plat_h, Raylib.GREEN);
+            // TODO: Piirrä aluksen kuva kolmion sijaan # 
+            Raylib.DrawTexture(texture,(int)x - 29,(int)y - 35, Raylib.WHITE);
+            // Piirrä moottorin liekki
+            if (engine_on)
+            {
+                Raylib.DrawTriangle(new Vector2(x - 5, y), new Vector2(x, y + 32), new Vector2(x + 5, y), Raylib.YELLOW);
+
+            }
+
+            // Piirrä polttoaineen tilanne
+            Raylib.DrawRectangle(9, 9, 102, 12, Raylib.BLUE);
+            Raylib.DrawRectangle(10, 10, (int)fuel, 10, Raylib.YELLOW);
+            Raylib.DrawText("FUEL", 11, 11, 12, Raylib.DARKBLUE);
+
+            // Piirrä debug tietoja
+            Raylib.DrawText($"V:{velocity}", 11, 31, 8, Raylib.WHITE);
+            Raylib.DrawText($"dt:{delta_time}", 11, 41, 8, Raylib.WHITE);
+
+            // TODO: Lopeta piirtäminen #
+            Raylib.EndTextureMode();
+        }
+    }
+}
--- Rogue/Game.cs
+++ Rogue/Game.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using ZeroElectric.Vinculum;
 using TurboMapReader;
+using RayGuiCreator;
+using System.Diagnostics;
 
 
 namespace Rogue
@@ -14,12 +16,37 @@ namespace Rogue
     internal class Game
     {
         PlayerCharacter player;
-        
+
+        TextBoxEntry playerNameEntry = new TextBoxEntry(10);
+
         Map lvl1;
 
+        MultipleChoiceEntry characterClass = new MultipleChoiceEntry(
+            new string[] { "A", "B", "C" });
 
+        MultipleChoiceEntry race = new MultipleChoiceEntry(
+       new string[] { "A", "B", "C" });
+        public enum GameState
+        {
+            MainMenu,
+            CharacterCreation,
+            GameLoop,
+            PauseMenu,
+            OptionsMenu,
+            Quit
+        }
+
+        //Pino
+        Stack<GameState> stateStack = new Stack<GameState>();
+
+        OptionsMenu myOptionsMenu;
+        PauseMenu myPauseMenu;
+
+        GameState currentGameState;
 
         Enemy enemy;
+
+        Sound soundToPlay;
 
         public static readonly int tileSize = 16;
 
@@ -27,18 +54,23 @@ namespace Rogue
         const int screen_height = 720;
         public void Run()
         {
+            //currentGameState = GameState.MainMenu;
+            stateStack.Push(GameState.MainMenu);
 
+            myOptionsMenu = new OptionsMenu();
+            myPauseMenu = new PauseMenu();
 
-
-
-
+            myOptionsMenu = new OptionsMenu();
+            // Kytke asetusvalikon tapahtumaan funktio
+            myOptionsMenu.BackButtonPressedEvent += this.OnOptionsBackButtonPressed;
+            myPauseMenu.BackButtonPressedEvent += this.OnPauseBackButtonPressed;
             // Prepare to show game
             Console.CursorVisible = false;
 
             // A small window
             Console.WindowWidth = 60;
-            
-            
+
+
             Console.WindowHeight = 26;
 
             // Create player
@@ -48,138 +80,27 @@ namespace Rogue
             MapReader reader = new MapReader();
 
 
-            
-           
-            
+
+
+
 
             player.position = new Vector2(3, 3);
 
-            while (true)
-            {
-
-                Console.WriteLine("Anna nimi:");
-                string nimi = Console.ReadLine();
-                bool ok = true;
-                foreach (char c in nimi)
-                {
-                    if (Char.IsDigit(c))
-                    {
-                        ok = false;
-
-                    }
-
-                }
-                if (ok == false)
-                {
-                    Console.WriteLine("Nimessä ei saa olla numeroita");
-
-                }
-                else
-                {
-                    nimi = player.nimi;
-                    break;
-                }
-            }
+           
 
 
-            while (true)
-            {
-                bool ok = true;
-                Console.WriteLine("Valitse rotu:");
-                Console.WriteLine("1: Rotu1");
-                Console.WriteLine("2: Rotu2");
-                Console.WriteLine("3: Rotu3");
+           
 
-                string vastaus = Console.ReadLine();
-
-                foreach (char c in vastaus)
-                {
-                    if (!Char.IsDigit(c))
-                    {
-                        ok = false;
-
-                    }
-
-                }
-                if (ok == false)
-                {
-                    Console.WriteLine("Sinun täytyy valita rodun numero 1-3");
-                    continue;
-                }
-                int rotunumero = Convert.ToInt32(vastaus);
-
-                if (rotunumero == 1 && ok)
-                {
-                    player.rotu = Race.A;
-                    break;
-                }
-
-                else if (rotunumero == 1 && ok)
-                {
-                    player.rotu = Race.B;
-                    break;
-                }
-
-                else if (rotunumero == 3 && ok)
-                {
-                    player.rotu = Race.C;
-                    break;
-                }
-                Console.WriteLine("Sinun täytyy valita rodun numero 1-3");
-            }
-
-            while (true)
-            {
-                bool ok = true;
-                Console.WriteLine("Valitse luokka:");
-                Console.WriteLine("1: LuokkaA");
-                Console.WriteLine("2: LuokkaB");
-                Console.WriteLine("3: LuokkaC");
-
-                string vastaus = Console.ReadLine();
-
-                foreach (char c in vastaus)
-                {
-                    if (!Char.IsDigit(c))
-                    {
-                        ok = false;
-
-                    }
-
-                }
-
-                if (ok == false)
-                {
-                    Console.WriteLine("Sinun täytyy valita luokan numero 1-3");
-                    continue;
-                }
-
-                int luokanumero = Convert.ToInt32(vastaus);
-                if (luokanumero == 1 && ok)
-                {
-                    player.hahmoluokka = Class.A;
-                    break;
-                }
-
-                else if (luokanumero == 2 && ok)
-                {
-                    player.hahmoluokka = Class.B;
-                    break;
-                }
-
-                else if (luokanumero == 3 && ok)
-                {
-                    player.hahmoluokka = Class.C;
-                    break;
-                }
-                Console.WriteLine("Sinun täytyy valita luokan numero 1-3");
-            }
+           
 
             // Init and run game loop until ESC is pressed
             Console.Clear();
 
             Raylib.InitWindow(screen_width, screen_height, "Raylib");
 
+            Raylib.InitAudioDevice();
+
+            Sound basicSound = Raylib.LoadSound("Sound/Bass Drum.wav");
 
             Texture imageTexture = Raylib.LoadTexture("Images/tilemap.png");
 
@@ -191,18 +112,71 @@ namespace Rogue
             lvl1.LoadEnemiesAndItems();
             player.SetImageAndIndex(imageTexture, 12, 109);
 
+            soundToPlay = basicSound;
 
-
-            while (Raylib.WindowShouldClose() == false)
-            {
+           
                 GameLoop();
-            }
+            
 
 
             Raylib.CloseWindow();
 
         }
 
+        private void DrawMainMenu()
+        {
+            // Tyhjennä ruutu ja aloita piirtäminen
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Raylib.BLACK);
+
+            // Laske ylimmän napin paikka ruudulla.
+            int button_width = 100;
+            int button_height = 20;
+            int button_x = Raylib.GetScreenWidth() / 2 - button_width / 2;
+            int button_y = Raylib.GetScreenHeight() / 2 - button_height / 2;
+
+            // Piirrä pelin nimi nappien yläpuolelle
+            RayGui.GuiLabel(new Rectangle(button_x, button_y - button_height * 2, button_width, button_height), "Rogue");
+
+            if (RayGui.GuiButton(new Rectangle(button_x, button_y
+                , button_width, button_height), "Start Game") == 1)
+            {
+                // Start the game
+                //currentGameState = GameState.CharacterCreation;
+                stateStack.Push(GameState.CharacterCreation);
+            }
+            // Piirrä seuraava nappula edellisen alapuolelle
+            button_y += button_height * 2;
+
+            if (RayGui.GuiButton(new Rectangle(button_x,
+                button_y,
+                button_width, button_height), "Options") == 1)
+            {
+                // Go to options somehow
+               // currentGameState = GameState.OptionsMenu;
+                stateStack.Push(GameState.OptionsMenu);
+            }
+
+            button_y += button_height * 2;
+
+            if (RayGui.GuiButton(new Rectangle(button_x,
+                button_y,
+                button_width, button_height), "Pause") == 1)
+            {
+                //currentGameState = GameState.PauseMenu;
+                stateStack.Push(GameState.PauseMenu);
+            }
+
+            button_y += button_height * 2;
+
+            if (RayGui.GuiButton(new Rectangle(button_x,
+                button_y,
+                button_width, button_height), "Quit") == 1)
+            {
+                stateStack.Push(GameState.Quit);
+            }
+            Raylib.EndDrawing();
+        }
         private void DrawRay()
         {
             Raylib.BeginDrawing();
@@ -213,95 +187,238 @@ namespace Rogue
             Raylib.EndDrawing();
         }
 
+        void OnOptionsBackButtonPressed(object sender, EventArgs args)
+        {
+            //currentGameState = GameState.MainMenu;
+            stateStack.Pop();
+        }
+
+        void OnPauseBackButtonPressed(object sender, Game.GameState newState)
+        {
+            if (newState == Game.GameState.GameLoop)
+            {
+                stateStack.Pop();
+            }
+            else
+            {
+                //currentGameState = GameState.MainMenu;
+                stateStack.Push(GameState.MainMenu);
+            }
+        }
+
         void GameLoop()
         {
-            while (true)
+            bool gameIsOn = true;
+            while (Raylib.WindowShouldClose() == false && gameIsOn)
             {
-                DrawRay();
-                Update();
-                Draw();
-                
-            }
-        }
-        
-        void Draw()
-        {
-            Console.Clear();
-            lvl1.DrawMap();
-            player.Draw();
-            
-        }
-
-
-        void Update()
-        {
-
-            Vector2 newPlace = player.position;
-            /*
-                ConsoleKeyInfo key = Console.ReadKey();
-               
-                switch (key.Key)
+                switch (stateStack.Peek())
                 {
+                    case GameState.MainMenu:
+                        // Tämä koodi on uutta
+                        DrawMainMenu();
+                        break;
+                    case GameState.CharacterCreation:
+                        int x = 0;
+                        int y = 40;
+                        int withd = 300;
+                        DrawCharacterMenu(x +Raylib.GetScreenWidth()/2 - withd, y, withd);
+                        break;
+                    case GameState.PauseMenu:
+                        myPauseMenu.DrawMenu();
+                        break;
+                    case GameState.OptionsMenu:
+                        myOptionsMenu.DrawMenu();
+                        break;
+                    case  GameState.GameLoop:
+                        // Tämä koodi on se mitä GameLoop() funktiossa oli ennen muutoksia
+                        DrawRay();
+                        Update();
+                        Draw();
 
-                    case ConsoleKey.UpArrow:
-                        newPlace.Y -= 1;
                         break;
-                    case ConsoleKey.DownArrow:
-                        newPlace.Y += 1;
+                    case GameState.Quit:
+                        gameIsOn = false;
                         break;
-                    case ConsoleKey.LeftArrow:
-                        newPlace.X -= 1;
-                        break;
-                    case ConsoleKey.RightArrow:
-                        newPlace.X += 1;
-                        break;
+                }
 
-                    case ConsoleKey.Escape:
-                        
-                        break;
-
-                    default:
-                        break;
-                };
-            */
-
-            if (Raylib.IsKeyPressed(KeyboardKey.KEY_UP))
-            {
-                newPlace.Y -= 1;
-            }
-            else if (Raylib.IsKeyPressed(KeyboardKey.KEY_DOWN))
-            {
-                newPlace.Y += 1;
-            }
-            else if (Raylib.IsKeyPressed(KeyboardKey.KEY_LEFT))
-            {
-                newPlace.X -= 1;
-            }
-            else if (Raylib.IsKeyPressed(KeyboardKey.KEY_RIGHT))
-            {
-                newPlace.X += 1;
             }
 
+            void Draw()
+            {
+                Console.Clear();
+                lvl1.DrawMap();
+                player.Draw();
 
+            }
 
-            if (lvl1.GetTileAtGround((int)newPlace.X, (int)newPlace.Y) == Map.MapTile.Floor)
+            void DrawCharacterMenu(int x, int y, int width)
+            {
+                Raylib.ClearBackground(Raylib.GetColor(((uint)RayGui.GuiGetStyle(((int)GuiControl.DEFAULT), ((int)GuiDefaultProperty.BACKGROUND_COLOR)))));
+                MenuCreator c = new MenuCreator(x, y, Raylib.GetScreenHeight() / 20, width);
+                c.Label("Create character");
+                c.Label("Name Character");
+
+                c.TextBox(playerNameEntry);
+                c.Label("Select class");
+                c.DropDown(characterClass);
+                c.Label("Select race");
+                c.DropDown(race);
+
+                if (c.Button("Start Game"))
                 {
-                    player.position = newPlace;
-
-                foreach (var item in lvl1.enemies)
-                {
-                    if (item.position == player.position)
+                    bool nameOk = true;
+                    var nimi = playerNameEntry.ToString();
+                    if (string.IsNullOrEmpty(nimi))
                     {
-                        Console.WriteLine($"You hit enemy {item.name}");
+
+                        Console.WriteLine("Ei kelpaa");
+                        nameOk = false;
                     }
+
+
+
+                    for (int i = 0; i < nimi.Length; i++)
+                    {
+                        char kirjain = nimi[i];
+
+                        if (char.IsLetter(kirjain))
+                        {
+
+                        }
+
+                        else
+                        {
+                            nameOk = false;
+                            break;
+                        }
+                    }
+                    string raceAnswer = race.ToString();
+                    string classAnswer = characterClass.ToString();
+
+                    bool raceSelect = false;
+                    bool classSelect = false;
+
+                    if (raceAnswer == "A")
+                    {
+                        raceSelect = true;
+                        player.rotu = Race.A;
+                    }
+                    if (raceAnswer == "B")
+                    {
+                        raceSelect = true;
+                        player.rotu = Race.B;
+                    }
+                    if (raceAnswer == "C")
+                    {
+                        player.rotu = Race.C;
+                        raceSelect = true;
+                    }
+                    if (classAnswer == "A")
+                    {
+                        player.hahmoluokka = Class.A;
+                        classSelect = true;
+                    }
+                    if (classAnswer == "B")
+                    {
+                        player.hahmoluokka = Class.B;
+                        classSelect = true;
+
+                    }
+                    if (classAnswer == "C")
+                    {
+                        player.hahmoluokka = Class.C;
+                        classSelect = true;
+
+                    }
+
+                    if (nameOk && raceSelect == true && classSelect == true) { stateStack.Push( currentGameState = GameState.GameLoop); }
+
+                }
+                c.EndMenu();
+                Raylib.EndDrawing();
+            }
+
+         
+
+
+            void Update()
+            {
+
+                Vector2 newPlace = player.position;
+                /*
+                    ConsoleKeyInfo key = Console.ReadKey();
+
+                    switch (key.Key)
+                    {
+
+                        case ConsoleKey.UpArrow:
+                            newPlace.Y -= 1;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            newPlace.Y += 1;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            newPlace.X -= 1;
+                            break;
+                        case ConsoleKey.RightArrow:
+                            newPlace.X += 1;
+                            break;
+
+                        case ConsoleKey.Escape:
+
+                            break;
+
+                        default:
+                            break;
+                    };
+                */
+
+                if (Raylib.IsKeyPressed(KeyboardKey.KEY_UP))
+                {
+                    Raylib.PlaySound(soundToPlay);
+                    newPlace.Y -= 1;
+                }
+                else if (Raylib.IsKeyPressed(KeyboardKey.KEY_DOWN))
+                {
+                    Raylib.PlaySound(soundToPlay);
+                    newPlace.Y += 1;
+                }
+                else if (Raylib.IsKeyPressed(KeyboardKey.KEY_LEFT))
+                {
+                    Raylib.PlaySound(soundToPlay);
+                    newPlace.X -= 1;
+                }
+                else if (Raylib.IsKeyPressed(KeyboardKey.KEY_RIGHT))
+                {
+                    Raylib.PlaySound(soundToPlay);
+                    newPlace.X += 1;
+                }
+                else if (Raylib.IsKeyPressed(KeyboardKey.KEY_TAB))
+                {
+                    stateStack.Push(GameState.PauseMenu);
+                    
                 }
 
 
-                foreach (var item in lvl1.items)
+                if (lvl1.GetTileAtGround((int)newPlace.X, (int)newPlace.Y) == Map.MapTile.Floor)
                 {
-                    if (item.position == player.position)
+                    player.position = newPlace;
+
+                    foreach (var item in lvl1.enemies)
                     {
-                        Console.WriteLine($"You hit item {item.name}");
+                        if (item.position == player.position)
+                        {
+                            Console.WriteLine($"You hit enemy {item.name}");
+                        }
+                    }
+
+
+                    foreach (var item in lvl1.items)
+                    {
+                        if (item.position == player.position)
+                        {
+                            Console.WriteLine($"You hit item {item.name}");
+                        }
                     }
                 }
             }
